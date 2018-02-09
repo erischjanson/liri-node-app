04f51c3ab20725a52987445ddff291a5f65ad567
Elizabeth <erischjanson@gmail.com>
2018-02-09 15:39:39 -0600

    added default omdb request if no movie title specified

--- liri.js
+++ liri.js
@@ -6,7 +6,7 @@ var twitter = require("twitter");
 var spotify = require("node-spotify-api");
 var keys = require("./keys.js");
 
-
+//takes in user's command and parameters(e.g. movie title)
 var command = process.argv[2];
 var commandParameters = "";
 for(var i = 3; i < process.argv.length; i++){
@@ -20,7 +20,7 @@ for(var i = 3; i < process.argv.length; i++){
 //console.log(keys);
 // console.log(keys.twitter);
 
-
+//twitter api call
 function tweet(){	
 	var client = new twitter({
 	  consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -46,15 +46,18 @@ function tweet(){
 
 };
 
+
+//spotify api call
 function spotifyMe(){
 	console.log("spotify");
 };
 
+//omdb api call
 function movie(){
 	console.log("movie");
 	//var title = process.argv
 	request("http://www.omdbapi.com/?t=" + commandParameters + "&y=&plot=short&apikey=trilogy", function(error,response,body){
-		if(!error && response.statusCode===200){
+		if(!error && response.statusCode===200 && commandParameters){
 			//console.log(JSON.parse(body));
 			console.log("Movie: " + JSON.parse(body).Title);
 			console.log("Release Year: " + JSON.parse(body).Year);
@@ -64,11 +67,24 @@ function movie(){
 			console.log("Language: " + JSON.parse(body).Language);
 			console.log("Plot: " + JSON.parse(body).Plot);
 			console.log("Actors: " + JSON.parse(body).Actors);
+		} else {
+			request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy", function(error, response, body){
+			console.log("Movie: " + JSON.parse(body).Title);
+			console.log("Release Year: " + JSON.parse(body).Year);
+			console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+			console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+			console.log("Country: " + JSON.parse(body).Country);
+			console.log("Language: " + JSON.parse(body).Language);
+			console.log("Plot: " + JSON.parse(body).Plot);
+			console.log("Actors: " + JSON.parse(body).Actors);
+			})
 		}
 	})
 
 };
 
+
+//file reading function
 function random(){
 	console.log("random");
 }
