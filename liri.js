4f6affa7717aa28b75fd99299a89a1c26521b5eb
Elizabeth <erischjanson@gmail.com>
2018-02-09 15:01:56 -0600

    default omdb search working

--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
-
+var fs = require("fs");
+var request = require("request");
 var twitter = require("twitter");
 var spotify = require("node-spotify-api");
 var keys = require("./keys.js");
@@ -25,6 +26,7 @@ function tweet(){
 	client.get("statuses/user_timeline", params, function(error, tweets, response) {
 	  if (!error) {
 	    for(var i =0; i < tweets.length; i++){
+	    	console.log(tweets[i]);
 	    	console.log(tweets[i].created_at);
 	    	console.log(tweets[i].text);
 	    }
@@ -41,6 +43,19 @@ function spotifyMe(){
 
 function movie(){
 	console.log("movie");
+	request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy", function(error,response,body){
+		if(!error && response.statusCode===200){
+			console.log("Movie: " + JSON.parse(body).Title);
+			console.log("Release Year: " + JSON.parse(body).Year);
+			console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+			console.log("Rotten Tomatoes Ratings: " + JSON.parse(body).Ratings[1].Value);
+			console.log("Country: " + JSON.parse(body).Country);
+			console.log("Language: " + JSON.parse(body).Language);
+			console.log("Plot: " + JSON.parse(body).Plot);
+			console.log("Actors: " + JSON.parse(body).Actors);
+		}
+	})
+
 };
 
 function random(){
